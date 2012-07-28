dd91b16522bfda923b9177bb076156a4bf8ab223
Yogthos <dmitri.sotnikov@gmail.com>
2012-07-28 10:59:18 -0400

    updated macro

--- src/yuggoth/views/util.clj
+++ src/yuggoth/views/util.clj
@@ -31,13 +31,13 @@
      (resp/redirect "/")))
 
 (defmacro cache [id content]
-  (if (session/get :admin)
-    content
-    `(let [cached# (get @cached ~id)
-           last-updated# (:time cached#)
-           cur-time# (.getTime (new java.util.Date))
-           cached-content# (:content cached# )]
-       (if (or (nil? last-updated#)
-               (> (- cur-time# last-updated#) 10000))
-         (send cached assoc ~id {:time cur-time# :content ~content}))
-       (or cached-content# ~content)))) 
+  `(if (session/get :admin)
+    ~content
+    (let [cached# (get @cached ~id)
+          last-updated# (:time cached#)
+          cur-time# (.getTime (new java.util.Date))
+          cached-content# (:content cached# )]
+      (if (or (nil? last-updated#)
+              (> (- cur-time# last-updated#) 10000))
+        (send cached assoc ~id {:time cur-time# :content ~content}))
+      (or cached-content# ~content)))) 
