0ee73f6dd71e81013a21e28602daab7692897b00
Yogthos <dmitri.sotnikov@gmail.com>
2012-08-02 23:56:08 -0400

    updated markdown-clj dependency

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
             :description "personal blog engine"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3" :exclude [org.clojure/clojure]]
-                           [markdown-clj "0.9.3"]
+                           [markdown-clj "0.9.4"]
                            [org.clojure/java.jdbc "0.2.3"]
                            [net.sf.jlue/jlue-core "1.3"]
                            [joda-time "2.0"]
                            [postgresql/postgresql "9.1-901.jdbc4"]]
-	    :dev-dependencies [[lein-ring "0.7.1"]]
-     :ring {:handler yuggoth.server/handler}
-     :main yuggoth.server)
+            :dev-dependencies [[lein-ring "0.7.1"]]
+            :ring {:handler yuggoth.server/handler}
+            :main yuggoth.server)
 
