1e7f278db83fc4a13cee914a508cdc2136bd3d8e
Yogthos <dmitri.sotnikov@gmail.com>
2012-08-04 15:33:59 -0400

    added rss image, improved rss content parsing

--- src/yuggoth/views/rss.clj
+++ src/yuggoth/views/rss.clj
@@ -8,28 +8,35 @@
   (:import java.util.Date))
 
 (def site-url "http://yogthos.net/")
+(def time-format "EEE, dd MMM yyyy HH:mm:ss ZZZZ")
 
 (defmacro tag [id attrs & content]
   `{:tag ~id :attrs ~attrs :content [~@content]})
 
+(defmacro swap [x y]
+  `(fn [~'s] (. ~'s replaceAll ~x ~y)))
+
+(defn fix-special-characters [input] 
+  (reduce (fn [s [x y]] ((swap x y) s)) 
+          input
+          [["&" "&amp;"] ["<" "&lt;"] [">" "&gt;"]]))
+
 (defn parse-content [content]
-  (.replaceAll
-    (.replaceAll 
-      (markdown/md-to-html-string (if (> (count content) 300) (str (.substring content 0 500) " [...]") content))
-      "<" "&lt;")
-    ">" "&gt;"))
+  (fix-special-characters (markdown/md-to-html-string (if (> (count content) 300) (str (.substring content 0 500) " [...]") content))))
 
 (defn gen-item [author {:keys [id title content time]}]
-  (tag :item nil 
-       (tag :title nil title)
-       (tag :dc:creator nil author)
-       (tag :description nil (parse-content content))
-       (tag :link nil (str site-url "blog/" id ))
-       (tag :pubDate nil (util/format-time time "EEE dd MMM yyyy HH:mm:ss ZZZZ"))
-       (tag :category nil "clojure")))
+  (let [link (str site-url "blog/" id )] 
+    (tag :item nil
+         (tag :guid nil link)
+         (tag :title nil title)
+         (tag :dc:creator nil author)
+         (tag :description nil (parse-content content))
+         (tag :link nil link)
+         (tag :pubDate nil (util/format-time time time-format))
+         (tag :category nil "clojure"))))
 
 (defn gen-message [{:keys [title handle]} posts]
-  (let [date (util/format-time (new Date) "EEE dd MMM yyyy HH:mm:ss ZZZZ")] 
+  (let [date (util/format-time (new Date) time-format)] 
     (tag :rss {:version "2.0"
                :xmlns:dc "http://purl.org/dc/elements/1.1/"
                :xmlns:sy "http://purl.org/rss/1.0/modules/syndication/"}
@@ -46,11 +53,11 @@
            [:content]
            into (map (partial gen-item handle) posts)))))
 
-(defn serve-feed [admin posts]
+(defn feed [admin posts]
   (.getBytes (with-out-str (xml/emit (gen-message admin posts)))))
 
 (defpage "/rss" []
   (resp/content-type 
     "application/rss+xml" 
     (new java.io.ByteArrayInputStream 
-         (serve-feed (db/get-admin) (db/get-posts 10 true)))))
+         (feed (db/get-admin) (db/get-posts 10 true)))))
