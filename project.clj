157f2c8534328d4dd40d80d1d9c20a7a2593669d
alexdforeman <@gmail.comalexdforeman>
2014-01-09 15:05:28 -0500

    Min version of leiningen to 2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject yuggoth "0.5.0-SNAPSHOT"
-  
+
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  
+  :min-lein-version "2.0.0"
+
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [lib-noir "0.6.4"]
@@ -13,18 +14,18 @@
    [com.taoensso/timbre "1.6.0"]
    [markdown-clj "0.9.25"]
    [net.sf.jlue/jlue-core "1.3"]
-   [org.clojure/java.jdbc "0.3.0-alpha4"]     
+   [org.clojure/java.jdbc "0.3.0-alpha4"]
    [postgresql/postgresql "9.1-901.jdbc4"]]
-  
+
   :ring
-  {:handler yuggoth.handler/war-handler, 
+  {:handler yuggoth.handler/war-handler,
    :init yuggoth.handler/init}
-  
+
   :profiles
-  {:production 
+  {:production
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
-   :dev 
+   :dev
    {:dependencies [[ring-mock "0.1.3"] [ring/ring-devel "1.1.8"]]}}
-   
+
   :plugins [[lein-ring "0.8.3"]])
