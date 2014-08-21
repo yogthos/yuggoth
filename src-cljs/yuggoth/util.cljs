3b149768e6df42f2830b45b8541293e3cbd43d63
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-08-21 11:49:45 -0400

    cleanup

--- src-cljs/yuggoth/util.cljs
+++ src-cljs/yuggoth/util.cljs
@@ -11,14 +11,14 @@
 (defn auth-hash [user pass]
   (->> (str user ":" pass) (js/btoa) (str "Basic ")))
 
-(defn mk-url [url]
-  (str js/context url "?time="(.getTime (js/Date.))))
+(defn millis []
+  (.getTime (js/Date.)))
 
 (defn GET [url params]
-  (ajax/GET (mk-url url) params))
+  (ajax/GET (str js/context url) (assoc params :time (millis))))
 
 (defn POST [url params]
-  (ajax/POST (mk-url url) params))
+  (ajax/POST (str js/context url "?time=" (millis)) params))
 
 (defn error-handler [response]
   (session/reset! {:error response}))
